21c81bdceb239e961f3b7dce9795381ef9050ce6
kyungaaaaaa <kyungaaaaaa1223@gmail.com>
2020-12-16 12:55:01 +0900

    session

--- src/example/Calc.java
+++ src/example/Calc.java
@@ -6,27 +6,29 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/calc")
 public class Calc extends HttpServlet {
     public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ServletContext application = request.getServletContext();
+//        ServletContext application = request.getServletContext();
+        HttpSession session = request.getSession();
         String v_ = request.getParameter("v");
         String op = request.getParameter("op");
         int v = 0;
         if (!v_.equals("")) v = Integer.parseInt(v_);
         if (op.equals("=")) {
 
-            int x = (Integer) application.getAttribute("value");
+            int x = (Integer) session.getAttribute("value");
             int y = v;
 
-            if (application.getAttribute("op").equals("+")) response.getWriter().println(x + y);
+            if (session.getAttribute("op").equals("+")) response.getWriter().println(x + y);
             else response.getWriter().println(x - y);
 
         } else {
-            application.setAttribute("value", v);
-            application.setAttribute("op", op);
+            session.setAttribute("value", v);
+            session.setAttribute("op", op);
         }
 
 
