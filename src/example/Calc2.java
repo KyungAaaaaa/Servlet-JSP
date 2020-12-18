7e1f1636ce5d08ead057a6817e16e3c9134cb440
kyungaaaaaa <kyungaaaaaa1223@gmail.com>
2020-12-18 19:48:04 +0900

    Example Project - 1
    DB connect
    detail.jsp -> MVC Pattern 1

--- src/example/Calc2.java
+++ src/example/Calc2.java
@@ -13,43 +13,7 @@ import java.io.IOException;
 
 @WebServlet(urlPatterns = "/calc2")
 public class Calc2 extends HttpServlet {
-    public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        String value = request.getParameter("value");
-        String op = request.getParameter("op");
-        String dot = request.getParameter("dot");
-        String exp = "";
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null)
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("exp")) {
-                    exp = cookie.getValue();
-                    break;
-                }
-            }
-        if (op != null && op.equals("=")) {
-            ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
-            try {
-                exp = String.valueOf(engine.eval(exp));
-            } catch (ScriptException e) {
-                e.printStackTrace();
-            }
-
-        }else if(op != null && op.equals("C")){
-            exp="";
-
-        } else {
-            exp += (value == null) ? "" : value;
-            exp += (op == null) ? "" : op;
-            exp += (dot == null) ? "" : dot;
-        }
-        Cookie expCookie = new Cookie("exp", exp);
-        if(op != null && op.equals("C")) expCookie.setMaxAge(0);
-        response.addCookie(expCookie);
-
-        response.sendRedirect("calcpage");
-
-
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
